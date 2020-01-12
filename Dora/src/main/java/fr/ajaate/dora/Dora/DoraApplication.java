a0380f20c6cecdabe4e7df5d7651d6fab1d0e50c
Aissa <aissa.bnhd@gmail.com>
2020-01-12 18:17:43 +0100

    Creation du projet

---
+++ Dora/src/main/java/fr/ajaate/dora/Dora/DoraApplication.java
@@ -0,0 +1,13 @@
+package fr.ajaate.dora.Dora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoraApplication.class, args);
+	}
+
+}
---
+++ Dora/src/test/java/fr/ajaate/dora/Dora/DoraApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ajaate.dora.Dora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
