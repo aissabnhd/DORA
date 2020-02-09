f1a15f1b932758896555d6faedec85fdb9c04267
Eseid BENMAMMAR <e2seid.admi@gmail.com>
2020-02-09 11:37:58 +0100

    create DMPServices interface, DMPServicesImplementation and DMPController

--- Dora/src/main/java/fr/ajaate/dora/dao/DMPRepository.java
+++ Dora/src/main/java/fr/ajaate/dora/dao/DMPRepository.java
@@ -4,6 +4,18 @@ import fr.ajaate.dora.entities.DMP;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface DMPRepository extends JpaRepository<DMP, Long> {
+
+    List<DMP> findAllByLastName(String lastName);
+
+    List<DMP> findAllByFirsName(String firstName);
+
+    Optional<DMP> findBySocialSecurityNumber(String socialSecurityNumber);
+
+    boolean existsDMPBySocialSecurityNumber(String socialSecurityNumber);
+
 }
---
+++ Dora/src/main/java/fr/ajaate/dora/services/DMPServices.java
@@ -0,0 +1,28 @@
+package fr.ajaate.dora.services;
+
+import fr.ajaate.dora.entities.DMP;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public interface DMPServices {
+
+    public DMP save(DMP dmp);
+
+    public DMP update(DMP dmp);
+
+    public List<DMP> findAll();
+
+    Optional<DMP> findById(Long id);
+
+    void deleteById(Long id);
+
+    public List<DMP> findByLastName(String lastName);
+
+    public List<DMP> findByFirstName(String firstName);
+
+    public Optional<DMP> findBySocialSecurityNumber(String socialSecurityNumber);
+
+
+}
