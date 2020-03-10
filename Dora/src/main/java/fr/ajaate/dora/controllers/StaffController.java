cce551533a5acf6dae4382c5e960ad9bb343fd77
jbenni <jaid.benni@gmail.com>
2020-03-10 19:12:33 +0100

    add findById method controller

--- Dora/src/main/java/fr/ajaate/dora/controllers/StaffController.java
+++ Dora/src/main/java/fr/ajaate/dora/controllers/StaffController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/Staff")
 public class StaffController {
@@ -20,6 +22,10 @@ public class StaffController {
         return new ResponseEntity<>(newStaff, HttpStatus.CREATED);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Staff> findById(@PathVariable("id") Long id){
+        return new ResponseEntity<Staff>(staffService.getOne(id).get(), HttpStatus.OK);
+    }
 
 
 }
